73f30cc4c9c2c0bdbd4a57673e3254207aeb4eea
Francesco Carnimeo <f.carnimeo@studenti.unipi.it>
2024-06-21 16:24:13 +0200

    renamed folders (fix)

---
+++ paroliere_srv/init.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void init(int port) {
+    loadMatrices();
+    initSocket(port);
+}
+
+void loadMatrices(char *filename, ...) {
+    
+}
+
+void initSocket(int port) {
+    // crea il socket
+    int server = socket(AF_INET, SOCK_STREAM, 0);
+    // controlla errori in fase di creazione del socket
+    if (server == -1) {
+        perror("Errore nella creazione del socket.");
+        exit(EXIT_FAILURE);
+    }
+    // imposta i parametri di rete per il server
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // accoppia il socket con indirizzo e porta
+    if (bind(server, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        perror("Errore in fase di accoppiamento del socket.");
+        exit(EXIT_FAILURE);
+    }
+    // mette il server in ascolto
+    if (listen(server, 10) == -1) {
+        perror("Errore in fase di ascolto del server.");
+        exit(EXIT_FAILURE);
+    }
+    printf("Server in ascolto sulla porta %d.\n", port);
+}

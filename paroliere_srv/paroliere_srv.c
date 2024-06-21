e8acff78a46f7367cb338420af5b847aea180ada
Francesco Carnimeo <f.carnimeo@studenti.unipi.it>
2024-06-21 18:09:13 +0200

    added serverState.h

--- paroliere_srv/paroliere_srv.c
+++ paroliere_srv/paroliere_srv.c
@@ -1,5 +1,14 @@
 #include "includes.h"
 
+// crea le mutex
+pthread_mutex_t currentWord_mtx = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t currentState_mtx = PTHREAD_MUTEX_INITIALIZER;
+// inizializza le variabili globali
+char currentWord[10] = "         ";
+ServerState currentState = WAIT_INIT;
+
+
+
 int main(void) {
     printf("Hello World!\n");
 }
---
+++ paroliere_srv/serverState.h
@@ -0,0 +1,11 @@
+#ifndef SERVERSTATE_H
+#define SERVERSTATE_H
+
+typedef enum {
+    INIT,
+    PLAY,
+    PAUSE,
+    ERROR    
+} ServerState;
+
+#endif SERVERSTATE_H

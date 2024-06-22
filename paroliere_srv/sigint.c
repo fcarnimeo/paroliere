589bed4cfe7e49d149a7aa349acc418cf93920c6
Francesco Carnimeo <f.carnimeo@studenti.unipi.it>
2024-06-22 19:18:37 +0200

    refactoring

--- paroliere_srv/sigint.c
+++
@@ -1,7 +0,0 @@
-#include "includes.h"
-
-void sigintHandler(int sig) {
-    printf("Ricevuto segnale %d - SIGINT - (CTRL-C)." 
-    "Inizio spegnimento del server.\n", sig);
-    currentState = EXIT;
-}

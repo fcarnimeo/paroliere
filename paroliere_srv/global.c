cac503aab665b61b9b0450aa4b2bff916d4f19b5
Francesco Carnimeo <f.carnimeo@studenti.unipi.it>
2024-06-22 23:05:59 +0200

    added global.c

---
+++ paroliere_srv/global.c
@@ -0,0 +1,4 @@
+currentWord_mtx = PTHREAD_MUTEX_INITIALIZER;
+currentState_mtx = PTHREAD_MUTEX_INITIALIZER;
+currentWord[10] = "         ";
+ServerState currentState = INIT;
--- paroliere_srv/global.h
+++ paroliere_srv/global.h
@@ -1,14 +1,14 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
-#include "includes.h"
 #include "serverState.h"
 
 // crea le mutex
 pthread_mutex_t currentWord_mtx = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t currentState_mtx = PTHREAD_MUTEX_INITIALIZER;
 // inizializza le variabili globali
-extern char currentWord[10] = "         ";
-extern volatile ServerState currentState = INIT;
+// TODO - extern?
+char currentWord[10] = "         ";
+volatile ServerState currentState = INIT;
 
 #endif // GLOBAL_H

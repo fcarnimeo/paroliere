2bec57252a9cd09b57d19f64b3a93f6a7312e1e4
Francesco Carnimeo <f.carnimeo@studenti.unipi.it>
2024-06-22 11:49:44 +0200

    ufixed printf with argc

--- paroliere_srv/paroliere_srv.c
+++ paroliere_srv/paroliere_srv.c
@@ -1,9 +1,12 @@
 #include "includes.h"
 
-int main(void) {
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        printf("Comando: %s nome_server porta_server [--matrici data_filename]\ [--durata durata_in_minuti] [--seed rnd_seed] [--diz dizionario]\ [--disconnetti-dopo tempo in minuti]\n", argv[0]);
     // catturo il segnale SIGINT generato da CTRL-D
     if (signal(SIGINT, sigintHandler) == SIG_ERR) {
-        perror("Errore fatale nello spegnimento del server. Interruzione immediata.\n");
+        perror("Errore fatale nello spegnimento del server. 
+        Interruzione immediata.\n");
         exit(EXIT_FAILURE);
     }
 

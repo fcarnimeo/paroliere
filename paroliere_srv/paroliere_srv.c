31f78e7dbf64b9ac96eda6037440be7564a54e78
Francesco Carnimeo <f.carnimeo@studenti.unipi.it>
2024-06-22 10:03:45 +0200

    added SIGINT handler to main

--- paroliere_srv/paroliere_srv.c
+++ paroliere_srv/paroliere_srv.c
@@ -1,6 +1,12 @@
 #include "includes.h"
 
 int main(void) {
+    // catturo il segnale SIGINT generato da CTRL-D
+    if (signal(SIGINT, sigintHandler) == SIG_ERR) {
+        perror("Errore fatale nella chiusura dei thread. Server interrotto bruscamente.\n");
+        exit(EXIT_FAILURE);
+    }
+
     // TODO - controllare il while (true) e initServer()
     while (true) {
         // TODO - numero porta da CLI

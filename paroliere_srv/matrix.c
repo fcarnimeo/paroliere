06b2551984afa4771fa1db68d98ab1de56a5b949
Francesco Carnimeo <f.carnimeo@studenti.unipi.it>
2024-06-24 13:36:44 +0200

    cleanup

--- paroliere_srv/matrix.c
+++ paroliere_srv/matrix.c
@@ -1,7 +1,7 @@
 #include "includes.h"
 
 // funzione ricorsiva DFS per trovare corrispondenze tra caratteri
-bool foundChr(char matrix[MATRIX_SIZE][MATRIX_SIZE], char *word, int row, int col, int chrCount, bool visited[MATRIX_SIZE][MATRIX_SIZE]) {
+bool foundChr(int matrix[MATRIX_SIZE][MATRIX_SIZE], char *word, int row, int col, int chrCount, bool visited[MATRIX_SIZE][MATRIX_SIZE]) {
     // caso base: processati correttamente tutti i caratteri
     if (chrCount == strlen(word))
         return true;
@@ -23,46 +23,20 @@ bool foundChr(char matrix[MATRIX_SIZE][MATRIX_SIZE], char *word, int row, int co
     return found;
 }
 
-int findWordInMatrix(char matrix[MATRIX_SIZE][MATRIX_SIZE], char *word) {
+// il valore di ritorno e' il punteggio della parola
+int findWord(int matrix[MATRIX_SIZE][MATRIX_SIZE], char *word) {
     // lunghezza minima 4 lettere
     if (strlen(word) < 4)
         return 0;
-
     // inizializza matrice lettere visitate
     bool visited[MATRIX_SIZE][MATRIX_SIZE] = {false};
-
+    // cerca parola a partire da ogni casella
     for (int i = 0; i < MATRIX_SIZE; i++) {
         for (int j = 0; j < MATRIX_SIZE; j++) {
             if (foundChr(matrix, word, i, j, 0, visited)) {
-                return strlen(word); // Return the length of the word as the score
+                return strlen(word); // punteggio parola
             }
         }
     }
     return 0; // parola non trovata
-}
-
-int main() {
-    char matrix[MATRIX_SIZE][MATRIX_SIZE] = {
-        {'A', 'B', 'C', 'E'},
-        {'S', 'F', 'C', 'S'},
-        {'A', 'D', 'E', 'E'},
-        {'B', 'C', 'D', 'F'}
-    };
-
-    char *words[] = {"ABCCED", "SEE", "ABCB", "FADE", "BAD"};
-    int totalScore = 0;
-
-    for (int i = 0; i < sizeof(words) / sizeof(words[0]); i++) {
-        int score = findWordInMatrix(matrix, words[i]);
-        if (score > 0) {
-            printf("Word '%s' found in the matrix, Score: %d\n", words[i], score);
-            totalScore += score;
-        } else {
-            printf("Word '%s' not found in the matrix\n", words[i]);
-        }
-    }
-
-    printf("Total Score: %d\n", totalScore);
-
-    return 0;
 }

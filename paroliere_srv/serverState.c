b250eecfbee581c7943ba40291599e48d50e2600
Francesco Carnimeo <f.carnimeo@studenti.unipi.it>
2024-06-28 23:13:09 +0200

    working alpha with SIGINT

--- paroliere_srv/serverState.c
+++ paroliere_srv/serverState.c
@@ -4,24 +4,23 @@ void *serverStateManager(void *args) {
     struct timespec timeToWait;
     struct timeval now;
 
+    // ottieni la mutex di stato, primissima iterazione
+    pthread_mutex_lock(&state_mutex);
     while (true) {
         // entra nello stato PLAYING
-        pthread_mutex_lock(&state_mutex);
         // sezione critica
         serverState = PLAYING;
         printf("Stato server: PLAYING\n");
         pthread_cond_broadcast(&state_cond);
         // imposta la durata dello stato PLAYING
         clock_gettime(CLOCK_REALTIME, &timeToWait);
-        timeToWait.tv_sec += durata * 60; // <durata> di attesa per lo stato PLAYING
+        timeToWait.tv_sec += durata * 2; // <durata> di attesa per lo stato PLAYING
         // imposta la pausa
-        pthread_cond_timedwait(&state_cond, &state_mutex, &timeToWait);
+        pthread_cond_timedwait(&state_cond, &state_mutex, &timeToWait); // mutex libera nel frattempo
         if (serverState == SHUTDOWN)
             break;
-        pthread_mutex_unlock(&state_mutex);
 
         // entra nello stato PAUSED
-        pthread_mutex_lock(&state_mutex);
         // sezione critica
         serverState = PAUSED;
         generateRandomMatrix(currentMatrix);
@@ -29,12 +28,14 @@ void *serverStateManager(void *args) {
         pthread_cond_broadcast(&state_cond);
         // imposta la durata dello stato PAUSED
         clock_gettime(CLOCK_REALTIME, &timeToWait);
-        timeToWait.tv_sec += 60; // 1 minuto di attesa per lo stato PAUSED
+        timeToWait.tv_sec += 2; // 1 minuto di attesa per lo stato PAUSED
         // imposta la pausa
-        pthread_cond_timedwait(&state_cond, &state_mutex, &timeToWait);
+        pthread_cond_timedwait(&state_cond, &state_mutex, &timeToWait); // mutex libera nel frattempo
         if (serverState == SHUTDOWN)
             break;
-        pthread_mutex_unlock(&state_mutex);
     }
+    // rilascia la mutex di stato
+    pthread_mutex_unlock(&state_mutex);
+
     printf("serverStateManager_thread terminato.\n");
 }
